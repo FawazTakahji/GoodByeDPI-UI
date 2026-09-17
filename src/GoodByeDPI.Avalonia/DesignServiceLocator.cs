f4605e6de2c308e1c393fd9d2699e3345f3f1c16
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2026-09-17 12:31:57 +0300

    Add design-time service locator and update startup

---
+++ src/GoodByeDPI.Avalonia/DesignServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoodByeDPI.Avalonia;
+
+public class DesignServiceLocator : MarkupExtension
+{
+    public required Type Type { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Ioc.Default.GetRequiredService(Type);
+    }
+}
--- src/GoodByeDPI.Avalonia/Program.cs
+++ src/GoodByeDPI.Avalonia/Program.cs
@@ -12,23 +12,23 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args)
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
     {
         Ioc.Default.ConfigureServices(
             new ServiceCollection()
                 .AddCoreServices()
                 .BuildServiceProvider());
 
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-    }
-
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
 #if DEBUG
             .WithDeveloperTools()
 #endif
             .WithInterFont()
             .LogToTrace();
+    }
 }
